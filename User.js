0cbb0ec2cdff3aaad15d1d973d899d24081172c6
M. Rikza Khoirul Umar <umarikzaa@student.ub.ac.id>
2025-05-30 03:28:36 +0700

    Create User.js

---
+++ User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['user', 'admin']
+  }
+});
+
+// Hash password sebelum save
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
